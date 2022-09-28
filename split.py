d8c75f5ae2fe55f7057a6fbefe55471ad08352de
azzansm <azzanasimaa@gmail.com>
2022-09-28 19:07:19 +0700

    exercise 1

---
+++ split.py
@@ -0,0 +1,7 @@
+bill = int(input("Enter amount of bill: "))
+people = int(input("Enter number of people: "))
+tips = int(input("Enter amount of tips (%): "))
+tip_amount = bill*(tips/100)
+print(f"Tip amount per person is ${tip_amount/people:.2f}")
+total_amount = ((bill + tip_amount)/people)
+print(f"Total amount per person is ${total_amount: .2f}")
