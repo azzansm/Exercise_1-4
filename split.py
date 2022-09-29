6d40e96c8a4d2d02db817972341fb9ab608fddf4
azzansm <azzanasimaa@gmail.com>
2022-09-29 17:40:17 +0700

    exercise 1 and 4

--- split.py
+++ split.py
@@ -4,4 +4,11 @@ tips = int(input("Enter amount of tips (%): "))
 tip_amount = bill*(tips/100)
 print(f"Tip amount per person is ${tip_amount/people:.2f}")
 total_amount = ((bill + tip_amount)/people)
-print(f"Total amount per person is ${total_amount: .2f}")
+print(f"Total amount per person is ${total_amount:.2f}")
+
+sec = int(input("Enter time in second: "))
+h = int(sec/3600)
+sec = sec%3600
+m = int(sec/60)
+s = int(sec%60)
+print("Result: ",h,":",m,":",s)
